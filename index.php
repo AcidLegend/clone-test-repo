158a5e57bf09a4779b2084f6fc82b9e3aac48491
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-06 18:56:51 +0100

    new update for index.php

--- index.php
+++ index.php
@@ -1,89 +1,75 @@
-<!-- HTML -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Meta Tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-commerce Website</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Error Page</title>
+    <meta name="description" content="Best School in Taraba Nigeria">
+    <meta name="keywords" content="School in Taraba, Amkadamiyya, Amkad">
+    <meta name="author" content="Bsc. Software Engineering: Abdullahi Abubakar Kabri">
+    <meta name="robots" content="index, follow">
+    <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=">
+    <!-- CDN and local stylesheets -->
+    <link rel="shortcut icon" href="assets/favicon/favicon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/gh/creativetimofficial/public-assets@master/soft-ui-design-system-builder/v1.0.7/assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/gh/creativetimofficial/public-assets@master/soft-ui-design-system-builder/v1.0.7/assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/css/soft-design-system-pro.min.css?v=1.1.1">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">E-commerce Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div class="container">
-        <h1 class="display-4">Featured Products</h1>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Product 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 1</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#" class="btn btn-primary">Buy Now</a>
-                    </div>
+<body class="bg-info-soft" style="font-family: 'Open Sans'">
+    <header>
+       <!-- Navbar Light -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-white z-index-3 py-3">
+            <div class="container">
+                <a class="navbar-brand" href="https://amkadamiyya.com.ng" rel="tooltip" data-placement="bottom">
+                Amkadamiyya
+                </a>
+                <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon mt-2">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </span>
+                </button>
+                <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
+                <ul class="navbar-nav navbar-nav-hover mx-auto">
+                    <li class="nav-item mx-2">
+                        <a href="https://amkadamiyya.com.ng" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" role="button">Home</a>
+                    </li>
+
+                    <li class="nav-item mx-2">
+                        <a href="https://amkadamiyya.com.ng/pages/admission/admission-require.php" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" role="button">Admissions</a>
+                    </li>
+
+                    <li class="nav-item mx-2">
+                        <a href="https://amkadamiyya.com.ng" class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" role="button">Docs</a>
+                    </li>
+                </ul>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Product 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 2</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#" class="btn btn-primary">Buy Now</a>
-                    </div>
+        </nav>
+        <!-- End Navbar -->
+    </header>
+    <main>
+        <section class="py-7">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-6 my-auto">
+                    <h1 class="display-1 text-bolder text-gradient text-danger">Error 404</h1>
+                    <h2>Erm. Page not found</h2>
+                    <p class="lead">Storage Alert. We suggest you to go to the homepage while we solve this issue.</p>
+                    <a type="button" class="btn bg-gradient-dark mt-4" href="https://amkadamiyya.com.ng">Go to Homepage</a>
+                </div>
+                <div class="col-lg-6 my-auto position-relative d-none d-lg-block">
+                    <img class="w-100 position-relative" src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/illustrations/error-404.png" alt="404-error">
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Product 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 3</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#" class="btn btn-primary">Buy Now</a>
-                    </div>
-                </div>
             </div>
-        </div>
-        <h1 class="display-4">Best Sellers</h1>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Product 4">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 4</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#" class="btn btn-primary">Buy Now</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img class="card-img-top" src="https://via.placeholder.com/300x200" alt="Product 5">
-                    <div class="card-body">
-                        <h5 class="card-title">Product 5</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#" class="btn btn-primary">Buy Now</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        </section>
+    </main>
+    <script src="https://kit.fontawesome.com/286d2a7519.js" crossorigin="anonymous"></script>
+    <script src="js/soft-ui-dashboard.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
